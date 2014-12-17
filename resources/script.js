581f72835b58043c740c4a6341e972d5f932c30f
Goose <goosemixes@gmail.com>
2014-12-16 23:26:32 -0500

    form, design, -animate
    Contact form added. Design made to appear more subtle and readable.
    Animate.css disabled on most sections because of link functionality
    break.

--- resources/script.js
+++ resources/script.js
@@ -10,119 +10,121 @@
 $(document).ready(function()
 {
 
-// portfolio timed event
-function isElementInViewport(elem) {
-    var $elem = $(elem);
+// DISABLED UNTIL ANIMATE.CSS LINK GLITCH IS RESOLVED!
 
-    // Get the scroll position of the page.
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    var viewportTop = $(scrollElem).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
+// // portfolio timed event
+// function isElementInViewport(elem) {
+//     var $elem = $(elem);
 
-    // Get the position of the element on the page.
-    var elemTop = Math.round( $elem.offset().top );
-    var elemBottom = elemTop + $elem.height();
+//     // Get the scroll position of the page.
+//     var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+//     var viewportTop = $(scrollElem).scrollTop();
+//     var viewportBottom = viewportTop + $(window).height();
 
-    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-}
+//     // Get the position of the element on the page.
+//     var elemTop = Math.round( $elem.offset().top );
+//     var elemBottom = elemTop + $elem.height();
 
-function fadeInCheckAnimation(see) {
-    var $see = $(see);
+//     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+// }
 
-    // If the animation has already been started
-    // if ($elem.hasClass('start')) return;
+// function fadeInCheckAnimation(see) {
+//     var $see = $(see);
 
-    if (isElementInViewport($see)) {
-        // Start the animation
-        // $elem.addClass('start');
-	$see.addClass('animated fadeIn');
+//     // If the animation has already been started
+//     // if ($elem.hasClass('start')) return;
 
-    }
-}
+//     if (isElementInViewport($see)) {
+//         // Start the animation
+//         // $elem.addClass('start');
+// 	$see.addClass('animated fadeIn');
 
-function fadeInLeftCheckAnimation(see) {
-    var $see = $(see);
+//     }
+// }
 
-    // If the animation has already been started
-    // if ($elem.hasClass('start')) return;
+// function fadeInLeftCheckAnimation(see) {
+//     var $see = $(see);
 
-    if (isElementInViewport($see)) {
-        // Start the animation
-        // $elem.addClass('start');
-	$see.addClass('animated fadeInLeft');
+//     // If the animation has already been started
+//     // if ($elem.hasClass('start')) return;
 
-    }
-}
+//     if (isElementInViewport($see)) {
+//         // Start the animation
+//         // $elem.addClass('start');
+// 	$see.addClass('animated fadeInLeft');
 
-function fadeInRightCheckAnimation(see) {
-    var $see = $(see);
+//     }
+// }
 
-    // If the animation has already been started
-    // if ($elem.hasClass('start')) return;
+// function fadeInRightCheckAnimation(see) {
+//     var $see = $(see);
 
-    if (isElementInViewport($see)) {
-        // Start the animation
-        // $elem.addClass('start');
-	$see.addClass('animated fadeInRight');
+//     // If the animation has already been started
+//     // if ($elem.hasClass('start')) return;
 
-    }
-}
+//     if (isElementInViewport($see)) {
+//         // Start the animation
+//         // $elem.addClass('start');
+// 	$see.addClass('animated fadeInRight');
 
-function fadeInUpCheckAnimation(see) {
-    var $see = $(see);
+//     }
+// }
 
-    // If the animation has already been started
-    // if ($elem.hasClass('start')) return;
+// function fadeInUpCheckAnimation(see) {
+//     var $see = $(see);
 
-    if (isElementInViewport($see)) {
-        // Start the animation
-        // $elem.addClass('start');
-	$see.addClass('animated fadeInUp');
+//     // If the animation has already been started
+//     // if ($elem.hasClass('start')) return;
 
-    }
-}
+//     if (isElementInViewport($see)) {
+//         // Start the animation
+//         // $elem.addClass('start');
+// 	$see.addClass('animated fadeInUp');
 
-// Capture scroll events
-$(window).scroll(function(){
-    fadeInCheckAnimation('#portfolio-heading');
-    fadeInLeftCheckAnimation('.skillsLeft');
-    fadeInRightCheckAnimation('.skillsRight');
-    fadeInUpCheckAnimation('.contact');
+//     }
+// }
 
-    fadeInLeftCheckAnimation('.radio-left');
-    fadeInLeftCheckAnimation('.gumbo-left');
-    fadeInLeftCheckAnimation('.alex-left');
-    // fadeInLeftCheckAnimation('.zen-left');
-    fadeInLeftCheckAnimation('.tribune-left');
-    // fadeInLeftCheckAnimation('.magazine-left');
+// // Capture scroll events
+// $(window).scroll(function(){
+//     fadeInCheckAnimation('#portfolio-heading');
+//     fadeInLeftCheckAnimation('.skillsLeft');
+//     fadeInRightCheckAnimation('.skillsRight');
+//     fadeInUpCheckAnimation('.contact');
 
-    fadeInRightCheckAnimation('.radio-right');
-    fadeInRightCheckAnimation('.gumbo-right');
-    fadeInRightCheckAnimation('.alex-right');
-    // fadeInRightCheckAnimation('.zen-right');
-    fadeInRightCheckAnimation('.tribune-right');
-    // fadeInRightCheckAnimation('.magazine-right');
+//     fadeInLeftCheckAnimation('.radio-left');
+//     fadeInLeftCheckAnimation('.gumbo-left');
+//     fadeInLeftCheckAnimation('.alex-left');
+//     // fadeInLeftCheckAnimation('.zen-left');
+//     fadeInLeftCheckAnimation('.tribune-left');
+//     // fadeInLeftCheckAnimation('.magazine-left');
 
-    // fadeInCheckAnimation('#idenity-heading');
-    // fadeInCheckAnimation('#response-heading');
-    // fadeInCheckAnimation('#html-heading');
-    // fadeInCheckAnimation('#jquery-heading');
-    // fadeInCheckAnimation('#ajax-heading');
-    // fadeInCheckAnimation('#php-heading');
-    // fadeInCheckAnimation('#seo-heading');
-    // fadeInCheckAnimation('#documented-heading');
-    // fadeInCheckAnimation('#more-heading');
-});
+//     fadeInRightCheckAnimation('.radio-right');
+//     fadeInRightCheckAnimation('.gumbo-right');
+//     fadeInRightCheckAnimation('.alex-right');
+//     // fadeInRightCheckAnimation('.zen-right');
+//     fadeInRightCheckAnimation('.tribune-right');
+//     // fadeInRightCheckAnimation('.magazine-right');
 
-// $(window).scroll(function() {
-// 	var scrollTop = $(window).scrollTop();
-// 	var divam = 1;
-// 	$(".intro").css({
-// 	  "background-position":"0px -"+scrollTop/divam+"px"     
-// 	});
-// 	  $(".intro").css({
-// 	     "margin-top":"-"+scrollTop+"px"
-// 	});
+//     // fadeInCheckAnimation('#idenity-heading');
+//     // fadeInCheckAnimation('#response-heading');
+//     // fadeInCheckAnimation('#html-heading');
+//     // fadeInCheckAnimation('#jquery-heading');
+//     // fadeInCheckAnimation('#ajax-heading');
+//     // fadeInCheckAnimation('#php-heading');
+//     // fadeInCheckAnimation('#seo-heading');
+//     // fadeInCheckAnimation('#documented-heading');
+//     // fadeInCheckAnimation('#more-heading');
 // });
+
+// // $(window).scroll(function() {
+// // 	var scrollTop = $(window).scrollTop();
+// // 	var divam = 1;
+// // 	$(".intro").css({
+// // 	  "background-position":"0px -"+scrollTop/divam+"px"     
+// // 	});
+// // 	  $(".intro").css({
+// // 	     "margin-top":"-"+scrollTop+"px"
+// // 	});
+// // });
 
 }); //End Document
