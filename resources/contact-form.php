5ccf390a1ee4ee2f9badce224ff70b8427ce5779
Goose <goosepostbox@gmail.com>
2017-12-29 16:57:31 -0500

    Update contact-form.php and tweak language
    contact-form.php is still very weak example of code, but it's very short
    and simple so not too bad.

--- resources/contact-form.php
+++ resources/contact-form.php
@@ -1,46 +1,53 @@
 <?php
 
-$errors = '';
-$myemail = 'goosepostbox@gmail.com';//<-----Put email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
- 
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
- 
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
+resume_form_send_mail();
 
-if( empty($errors))
-{
- 
-$to = $myemail;
- 
-$email_subject = "WORK OFFER: $name";
- 
-$email_body = "Sent from the gooseweb.io form. ".
- 
-" Here are the details:\n Name: $name \n ".
- 
-"Email: $email_address\n Message \n $message";
- 
-$headers = "From: $myemail\n";
- 
-$headers .= "Reply-To: $email_address";
- 
-@mail($to,$email_subject,$email_body,$headers);
- 
-header('Location: ../');
- 
-}
+function resume_form_send_mail() {
+	// Honeypot
+	if (isset($_POST['bee_movie']) && $_POST['bee_movie']) {
+		echo 'You appear to be a robot';
+		return false;
+	}
 
-?>
+	// Inputs required
+	if (!isset($_POST['name'])  || !isset($_POST['email']) || !isset($_POST['message'])) {
+		echo 'Name, Email, and Message are required fields';
+		return false;
+	}
+
+	// Set inputs
+	$name = $_POST['name']; 
+	$email = $_POST['email']; 
+	$message = $_POST['message']; 
+
+	// Validate from email
+	$email_regex = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i";
+	if (!preg_match($email_regex, $email)) {
+		echo 'Error: Invalid email address';
+		return false;
+	}
+
+	// My email
+	$to = 'goosepostbox@gmail.com';
+
+	// Subject
+	$email_subject = 'Resume Form: ' . $name;
+
+	// Body
+	$email_body = '';
+	$email_body .= 'Sent from the resume form  - ';
+	$email_body .= 'Name: ' . $name . ' - ';
+	$email_body .= 'Email: ' . $email . ' - ';
+	$email_body .= 'Message: ' . $message . ' - ';
+
+	// Headers
+	$headers = '';
+	$headers .= 'From: ' . $to . ' - ';
+	$headers .= 'Reply-To: ' . $email;
+
+	// PHPMailer would be better but this will do
+	mail($to, $email_subject, $email_body, $headers);
+
+	// Redirect
+	header('Location: ../');
+}
