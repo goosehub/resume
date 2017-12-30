6c2963c772487d893bb67b9f097bdb830c1735bc
Goose <goosepostbox@gmail.com>
2017-12-29 21:19:56 -0500

    typed.js for the h2 tag
    This really brings the room together.

--- resources/script.js
+++ resources/script.js
@@ -20,3 +20,17 @@ function initMap() {
     });
     workArea.setMap(map);
 }
+
+$(function(){
+    $(".typed").typed({
+        strings: ["Developer", "Engineer", "Architect"],
+        typeSpeed: 30,
+        startDelay: 1200,
+        backSpeed: 20,
+        backDelay: 2000,
+        loop: true,
+        loopCount: false,
+        showCursor: true,
+        cursorChar: "|",
+    });
+});
